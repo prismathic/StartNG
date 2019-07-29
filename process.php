d3fafcf04d486ae5b35555702104c0684a6840e1
Seyi Onifade <xyluz@ymail.com>
2019-07-29 10:18:24 +0100

    basic data.json instead

--- process.php
+++ process.php
@@ -1,9 +1,6 @@
 <?php     
 
-$file = preg_grep('/^([^.])/', scandir('data', SCANDIR_SORT_DESCENDING));
-$path = __DIR__ . "/Data/" . $file[0];
-
-file_put_contents($path, json_encode($_POST),FILE_APPEND);
+file_put_contents("data.json", json_encode($_POST),FILE_APPEND);
 file_put_contents("email.txt", $_POST['email']."\r\n",FILE_APPEND);
 
 header("Location: success.html");
