3efa623e5863ffc1bd736eceecc6580e8cfda77e
Seyi Onifade <xyluz@ymail.com>
2019-07-29 10:20:54 +0100

    drop to next line

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php     
 
-file_put_contents("data.json", json_encode($_POST),FILE_APPEND);
+file_put_contents("data.json", json_encode($_POST)."\r\n",FILE_APPEND);
 file_put_contents("email.txt", $_POST['email']."\r\n",FILE_APPEND);
 
 header("Location: success.html");
