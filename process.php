246d238cb0df965977d8e425d16b179d913b1219
prismathic <pjjsalam1999@gmail.com>
2019-08-07 17:43:21 +0100

    Set validation against multiple registration

--- process.php
+++ process.php
@@ -1,7 +1,14 @@
 <?php  session_start();    
+if (strpos(file_get_contents("email.txt"),$_POST['email']) !== false) {
+    file_put_contents("data.json", json_encode($_POST)."\r\n",FILE_APPEND);
+    file_put_contents("email.txt", $_POST['email']."\r\n",FILE_APPEND);
+    $_SESSION['submit'] = "done";
+    header("Location: index.php");
+}
+else {
+    $_SESSION['submit'] = "undone";
+    header("Location: index.php");
+}
 
-file_put_contents("data.json", json_encode($_POST)."\r\n",FILE_APPEND);
-file_put_contents("email.txt", $_POST['email']."\r\n",FILE_APPEND);
-$_SESSION['submit'] = "done";
-header("Location: index.php");
+
 
